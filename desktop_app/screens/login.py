721ded5f30247fd5bd6887a3f80aa82d641255cb
Franklin G <136905574+000X00x0@users.noreply.github.com>
2025-05-17 15:20:50 -0500

    Update login.py

--- desktop_app/screens/login.py
+++ desktop_app/screens/login.py
@@ -1,5 +1,8 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+import requests
+from ttkbootstrap.dialogs import Messagebox
+
 
 class LoginScreen(ttk.Frame):
     def __init__(self, parent, controller):
@@ -7,6 +10,28 @@ class LoginScreen(ttk.Frame):
         self.controller = controller
         self.create_widgets()
 
+    def handle_login(self):
+        email = self.entry_mail.get().strip()
+        password = self.entry_password.get().strip()
+
+        if not email or not password:
+            Messagebox.show_error("Todos los campos son obligatorios", "Error")
+            return
+
+        try:
+            response = requests.post("http://localhost:8000/login", json={
+                "email": email,
+                "password": password
+            })
+            response.raise_for_status()  # Lanza un error si la respuesta no es 200
+            Messagebox.show_info("Login exitoso", "Éxito")
+            # Aquí puedes redirigir a otra pantalla después de un login exitoso
+        except requests.exceptions.RequestException as e:
+            try: 
+                error_message = e.response.json().get("Error desconocido", "Error desconocido")
+            except:
+                error_message = str(e)
+            Messagebox.show_error("Error: ", error_message)
     def create_widgets(self):
         form_frame = ttk.Frame(self)
         form_frame.pack(padx=10, pady=10)
@@ -22,4 +47,21 @@ class LoginScreen(ttk.Frame):
         ttk.Label(form_frame, text="Ingrese su contraseña").grid(row=4, column=1, pady=10)
         self.entry_password = ttk.Entry(form_frame, show="*", width=30)
         self.entry_password.grid(row=5, column=1, pady=5)
+        # boton de ingresar 
+        self.btn = ttk.Button(
+            form_frame,
+            text="Ingresar",
+            bootstyle=SUCCESS,
+            command=self.handle_login
 
+        )
+        self.btn.grid(row=6, column=1, pady=5)
+        # boton de registrarse
+        self.btn = ttk.Button(
+            form_frame,
+            text="Registrarse",
+            bootstyle=SUCCESS,
+            command=lambda: self.controller.show_screens("signup")
+        )
+        self.btn.grid(row=7, column=1, pady=5)
+        
