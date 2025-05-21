1b8db55b80d71c3de9f4557ce5756639e39018c3
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2025-05-20 20:43:50 -0500

    cambio en database.py

--- Backend/database.py
+++ Backend/database.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect (
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("192.168.0.11"),
+        user=os.getenv("Admin"),
+        password=os.getenv("Jorgeluis9"),
+        database=os.getenv("ProyectoReservas")
     )
